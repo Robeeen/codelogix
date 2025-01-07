adc93607c923534b1776d9b7a31151ecb680eacd
Robeeen <robeeen@gmail.com>
2025-01-07 20:26:56 +0600

    Custom Logo

--- header.php
+++ header.php
@@ -27,7 +27,13 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
+			//Custom Logo - 01.07.2025
+			if ( function_exists( 'the_custom_logo' ) ) {
+				the_custom_logo();
+			}
+
+
+
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
