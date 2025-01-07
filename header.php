148107ec9805f31243958862a9124d1d8034fce4
Robeeen <robeeen@gmail.com>
2025-01-08 00:40:11 +0600

    Header Size full width

--- header.php
+++ header.php
@@ -25,43 +25,45 @@
 <div id="page" class="site">
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			//Custom Logo - 01.07.2025 - https://developer.wordpress.org/themes/functionality/custom-logo/
-			if ( function_exists( 'the_custom_logo' ) ) {
-				the_custom_logo();
-			}
-
-
-
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<div class="the_header">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$codelogix_description = get_bloginfo( 'description', 'display' );
-			if ( $codelogix_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $codelogix_description;  ?></p>
-			<?php endif; ?>
+				//Custom Logo - 01.07.2025 - https://developer.wordpress.org/themes/functionality/custom-logo/
+				if ( function_exists( 'the_custom_logo' ) ) {
+					the_custom_logo();
+				}
+
+
+
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$codelogix_description = get_bloginfo( 'description', 'display' );
+				if ( $codelogix_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $codelogix_description;  ?></p>
+				<?php endif; ?>
+				
+			</div><!-- .site-branding -->
 			
-		</div><!-- .site-branding -->
-		
-		<div class="site-navigation">
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'codelogix' ); ?></button>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
-				?>
-			</nav><!-- #site-navigation -->
+			<div class="site-navigation">
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'codelogix' ); ?></button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
+			</div>
 		</div>
 	</header><!-- #masthead -->
