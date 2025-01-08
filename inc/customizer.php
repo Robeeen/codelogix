773af48b8033ecc852e0d43d8614db1ae8267c99
Robeeen <robeeen@gmail.com>
2025-01-08 19:25:58 +0600

    Logo padding settings added

--- inc/customizer.php
+++ inc/customizer.php
@@ -19,6 +19,13 @@ function codelogix_customize_register($wp_customize){
        'transport'         => 'refresh',
    ));
 
+    // Add setting for Padding top-bottom
+    $wp_customize->add_setting('custom_logo_padding', array(
+        'default'           => 5, // Default width
+        'sanitize_callback' => 'absint', // Ensure it's an integer
+        'transport'         => 'refresh',
+    ));
+
    // Add control (input field) for logo width
    $wp_customize->add_control('custom_logo_width_control', array(
        'label'    => __('Logo Width (px)', 'codelogix'),
@@ -31,17 +38,35 @@ function codelogix_customize_register($wp_customize){
            'step' => 10,  // Step value
        ),
    ));
+
+   // Add control (input field) for Paddding of Logo
+   $wp_customize->add_control('custom_logo_padding_control', array(
+    'label'    => __('Logo Padding (px)', 'codelogix'),
+    'section'  => 'custom_logo_section',
+    'settings' => 'custom_logo_padding',
+    'type'     => 'number',
+    'input_attrs' => array(
+        'min'  => 5,  // Minimum width
+        'max'  => 100, // Maximum width
+        'step' => 1,  // Step value
+    ),
+));
+
+
 }
 
 
 //Apply the Custom Width to the Logo via CSS
 function custom_logo_dynamic_css() {
    $logo_width = get_theme_mod('custom_logo_width', 200); // Get user-defined width
+   $logo_padding = get_theme_mod('custom_logo_padding', 200); //Get user-defined padding
    ?>
    <style>
        .custom-logo {
            max-width: <?php echo esc_attr($logo_width); ?>px;
            height: auto;
+           padding-top: <?php echo esc_attr($logo_padding); ?>px;
+           padding-bottom: <?php echo esc_attr($logo_padding); ?>px;
        }
    </style>
    <?php
@@ -49,7 +74,7 @@ function custom_logo_dynamic_css() {
 add_action('wp_head', 'custom_logo_dynamic_css');
 
 
-//Header Background Color 
+//Custom Header Background COLOR 
 
 add_action( 'customize_register', 'codelogix_customize_header_color' );
 
@@ -81,10 +106,12 @@ function custom_header_color_css() {
     ?>
     <style>
         #masthead {
-            background: <?php echo esc_attr($background_color);?>;
-            
+            background: <?php echo esc_attr($background_color);?>;            
         }
     </style>
     <?php
  }
  add_action('wp_head', 'custom_header_color_css');
+
+
+ 
