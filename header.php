944eeaa51cbbf792fff13145a270c734340094c4
Robeeen <robeeen@gmail.com>
2025-01-14 20:02:09 +0600

    Header Top Section Design

--- header.php
+++ header.php
@@ -36,12 +36,10 @@
 				<div class="top_middle"><i class="fa fa-phone"></i> <?php echo get_option('theme_fields1');?></div>
 				<div class="top_right"><i class="fab fa-whatsapp"></i> <?php echo get_option('theme_fields2');?></div>
 				<div class="top_social"> 
-					<i class="fab fa-github"></i>
-					<i class="fab fa-wordpress"></i>
-					<i class="fab fa-youtube"></i>
-					<i class="fab fa-facebook"></i>
-                    <i class="fab fa-linkedin"></i>
-					<i class="fab fa-whatsapp"></i>   
+				<i class="fab fa-linkedin"></i>
+					<i class="fab fa-youtube"></i> 
+					<i class="fab fa-facebook"></i> 
+                      
 
 				</div>
 			</div>
