ae42e1825119f0786e35eba08ffef798fae57e14
Robeeen <robeeen@gmail.com>
2025-01-20 11:00:52 +0600

    Import from a File coding-start

--- inc/admin/theme-customize.php
+++ inc/admin/theme-customize.php
@@ -39,7 +39,9 @@ function theme_settings_page(){
                 settings_fields('theme_settings_group');
                 do_settings_sections('theme_customization');
                 submit_button();
-                echo '<input type="reset" name="reset" id="reset" class="button button-primary" value="Reset Fields"  />';
+                echo '<p class="submit">';
+                echo '<input type="reset" name="reset" id="reset" class="button button-primary" value="Reset Values"  />';
+                echo '</p>';
                 echo '</form>';
             } elseif ($tab == 'advanced') {
                 echo '<h2>Header Settings</h2>';
@@ -60,7 +62,7 @@ function theme_settings_page(){
                 echo '<form method="post" action="options.php">';
                 settings_fields('export_settings_group');
                 do_settings_sections('export_settings');
-                //submit_button();
+                submit_button();
                 echo '</form>'; ?>
                 <!-- Export Button -->
                 <h2>Export all Settings Config</h2>
@@ -94,57 +96,6 @@ function theme_settings_page(){
     </div>
     <?php
 }
-
-//GET All option to Export
-add_action('admin_init', 'handle_settings_export');
-function handle_settings_export() {
-    if (isset($_POST['export_settings']) && check_admin_referer('export_settings', 'export_nonce')) {
-        $options = [
-            'custom_email' => get_option('custom_email'),
-            'custom_phone' => get_option('custom_phone'),
-            'custom_whatsapp' => get_option('custom_whatsapp'),
-            'custom_bg_color' => get_option('custom_bg_color'),
-            'custom_font_size' => get_option('custom_font_size'),
-            'custom_font_color' => get_option('custom_font_color'),
-            'custom_top_bar_height' => get_option('custom_top_bar_height'),
-            'top_social_color' => get_option('top_social_color'),
-            'fb_link' => get_option('fb_link'),
-            'linkedin_link' => get_option('linkedin_link'),
-            'tube_link' => get_option('tube_link'),
-            'twiter_link' => get_option('twiter_link'),
-            'skype_link' => get_option('skype_link'),
-            'toggle_switch' => get_option('toggle_switch'),
-            'custom_header_background' => get_option('custom_header_background'),
-            'header_border_bottom' => get_option('header_border_bottom'),
-            'border_background' => get_option('border_background'),
-            'button_text' => get_option('button_text'),
-            'menu_font_size' => get_option('menu_font_size'),
-            'menu_space' => get_option('menu_space'),
-            'site_logo' => get_option('site_logo'),
-            'custom_logo_size' => get_option('custom_logo_size'),
-            'custom_logo_padding' => get_option('custom_logo_padding'),
-            'button_background' => get_option('button_background'),
-            'button_text_color' => get_option('button_text_color'),
-            'button_padding' => get_option('button_padding'),
-            'button_font_size' => get_option('button_font_size'),
-            'button_border' => get_option('button_border'),
-            'button_radius' => get_option('button_radius'),
-            'button_border_color' => get_option('button_border_color'),
-            'button_font_family' => get_option('button_font_family'),
-            'button_switch' => get_option('button_switch'),          
-        ];
-
-        header('Content-Disposition: attachment; filename="settings-export.json"');
-        header('Content-Type: application/json');
-        echo json_encode($options, JSON_PRETTY_PRINT);
-        exit;
-    }
-}
-
-function my_admin_notices() {
-    settings_errors('general');
-}
-add_action('admin_notices', 'my_admin_notices');
 
 
 
@@ -1062,9 +1013,89 @@ function display_top_bar_height(){
    <?php
 }
 
-/*********************************END********************************************/
+/*********************************EXPORT - IMPORT ********************************************/
+//GET All option to Export
+add_action('admin_init', 'handle_settings_export');
+function handle_settings_export() {
+    if (isset($_POST['export_settings']) && check_admin_referer('export_settings', 'export_nonce')) {
+        $options = [
+            'custom_email' => get_option('custom_email'),
+            'custom_phone' => get_option('custom_phone'),
+            'custom_whatsapp' => get_option('custom_whatsapp'),
+            'custom_bg_color' => get_option('custom_bg_color'),
+            'custom_font_size' => get_option('custom_font_size'),
+            'custom_font_color' => get_option('custom_font_color'),
+            'custom_top_bar_height' => get_option('custom_top_bar_height'),
+            'top_social_color' => get_option('top_social_color'),
+            'fb_link' => get_option('fb_link'),
+            'linkedin_link' => get_option('linkedin_link'),
+            'tube_link' => get_option('tube_link'),
+            'twiter_link' => get_option('twiter_link'),
+            'skype_link' => get_option('skype_link'),
+            'toggle_switch' => get_option('toggle_switch'),
+            'custom_header_background' => get_option('custom_header_background'),
+            'header_border_bottom' => get_option('header_border_bottom'),
+            'border_background' => get_option('border_background'),
+            'button_text' => get_option('button_text'),
+            'menu_font_size' => get_option('menu_font_size'),
+            'menu_space' => get_option('menu_space'),
+            'site_logo' => get_option('site_logo'),
+            'custom_logo_size' => get_option('custom_logo_size'),
+            'custom_logo_padding' => get_option('custom_logo_padding'),
+            'button_background' => get_option('button_background'),
+            'button_text_color' => get_option('button_text_color'),
+            'button_padding' => get_option('button_padding'),
+            'button_font_size' => get_option('button_font_size'),
+            'button_border' => get_option('button_border'),
+            'button_radius' => get_option('button_radius'),
+            'button_border_color' => get_option('button_border_color'),
+            'button_font_family' => get_option('button_font_family'),
+            'button_switch' => get_option('button_switch'),          
+        ];
+
+        header('Content-Disposition: attachment; filename="settings-export.json"');
+        header('Content-Type: application/json');
+        echo json_encode($options, JSON_PRETTY_PRINT);
+        exit;
+    }
+}
+
+add_action('admin_notices', 'my_admin_notices');
+function my_admin_notices() {
+    settings_errors('export_import');
+}
 
 
+//FUnction to Import from a File
+add_action('admin_init', 'handle_settings_import');
+
+function handle_settings_import(){
+    if(isset($_GET['import_settings']) && check_admin_referer('import_settings', 'import_nonce')){
+        //If File is not empty
+        if(!empty($_FILES['settings-export']['tmp_name'])){
+            $file_content = file_get_contents($_FILES['settings-export']['tmp_name']);
+            $settings = json_decode($file_content, true);
+        }
+
+        if(json_last_error() === JSON_ERROR_NONE){
+            foreach($settings as $key => $value){
+                update_option($key, $value);
+            }
+            add_settings_error(
+                'export_import', 
+                'settings_imported', 
+                'Settings imported successfully!', 
+                'updated'
+            );
+        }
+    }else{
+        add_settings_error(
+            'export_import',
+            'settings_import_error', 
+            'Invalid JSON file.', 
+            'error');
+    }
+}
 
 
 
