64c6515e9dbab271b4a13a2c5a4d205da395dc2e
Robeeen <robeeen@gmail.com>
2025-01-24 11:11:45 +0600

    Fixing issue with Import from a json file

--- inc/admin/theme-customize.php
+++ inc/admin/theme-customize.php
@@ -1066,11 +1066,31 @@ function handle_settings_export() {
 add_action('admin_init', 'handle_settings_import');
 
 function handle_settings_import(){
-    if(isset($_GET['import_settings']) && check_admin_referer('import_settings', 'import_nonce')){
+    if(isset($_POST['import_settings']) && check_admin_referer('import_settings', 'import_nonce')){
         //If File is not empty
         if(!empty($_FILES['settings_file']['tmp_name'])){
+            $file_type = wp_check_filetype($_FILES['settings_file']['name']);
+            //We have added mime Type json after this code block
+            if ($file_type['ext'] !== 'json') {
+                add_settings_error(
+                    'export_import', 
+                    'file_type_error', 
+                    'Invalid file type. Please upload a JSON file.', 
+                    'error');
+                return;
+            }
+
             $file_content = file_get_contents($_FILES['settings_file']['tmp_name']);
+            if(!$file_content){
+                add_settings_error(
+                    'export_import',
+                    'file_read_error',
+                    'Unable to read the file',
+                    'error',
+                );
+            }
             $settings = json_decode($file_content, true);
+            
         }
 
         if(json_last_error() === JSON_ERROR_NONE){
@@ -1083,18 +1103,31 @@ function handle_settings_import(){
                 'Settings imported successfully!', 
                 'updated'
             );
+        }else{
+            add_settings_error(
+                'export_import',
+                'json_decode_error', 
+                'Invalid JSON file. Error: ' . json_last_error_msg(), 
+                'error');
         }
-    }else{
+    }else {
         add_settings_error(
-            'export_import',
-            'settings_import_error', 
-            'Invalid JSON file.', 
+            'export_import', 
+            'file_upload_error', 
+            'No file uploaded.', 
             'error');
     }
 }
 
+function allow_json_uploads($mime_types) {
+    $mime_types['json'] = 'application/json'; // Add JSON MIME type
+    return $mime_types;
+}
+add_filter('upload_mimes', 'allow_json_uploads');
+
 
 add_action('admin_notices', 'my_admin_notices');
+
 function my_admin_notices() {
     settings_errors('export_import');
 }
